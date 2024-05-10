c502bb4353f039fcef87fe0ecb3608b1e50cd81d
Obriena25 <obrienai@merrimack.edu>
2024-05-10 00:29:47 -0700

    added some exception handling and created test case for workflow two

--- src/ConnectionManager.java
+++ src/ConnectionManager.java
@@ -5,12 +5,24 @@ import java.sql.SQLException;
 public class ConnectionManager {
 
     private Connection connection;
+    boolean isValid;
 
     /**
      * Default constructor
      */
-    public ConnectionManager(String databaseName, String user, String password) throws SQLException {
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + databaseName, user, password);
+    public ConnectionManager(String databaseName, String user, String password) {
+        this.isValid = true;
+        try
+        {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + databaseName, user, password);
+        }
+        catch(SQLException e){
+            this.isValid = false;
+        }
+    }
+
+    public boolean isValid(){
+        return this.isValid;
     }
 
     public Connection getConnection() {
--- src/PresentationLayer.java
+++ src/PresentationLayer.java
@@ -3,19 +3,30 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 public class PresentationLayer {
+
     private static ConnectionManager getConnectionManager() throws SQLException {
         Scanner credentialScanner = new Scanner(System.in);
-        System.out.print("Enter username: ");
-        String userName = credentialScanner.nextLine();
-        System.out.print("Enter password: ");
         Console console = System.console();
-        char[] password = console.readPassword();
-        return new ConnectionManager("VideoGameShop", userName, new String(password));
+
+        while(true)
+        {
+            System.out.print("Enter username: ");
+            String userName = credentialScanner.nextLine();
+            System.out.print("Enter password: ");
+            char[] password = console.readPassword();
+            ConnectionManager connectMan = new ConnectionManager("VideoGameShop", userName, new String(password));
+            if(connectMan.isValid()){
+                return connectMan;
+            }
+            System.err.println("Incorrect username or password.");
+        }
     }
 
     public static void main(String[] args) throws SQLException {
+        Scanner optionScanner = new Scanner(System.in);
         ConnectionManager connectionManager = getConnectionManager();
-        var userOption = "";
+
+        String userOption = "";
         while (!userOption.equals("6")) {
             System.out.println();
             System.out.println("===============================================");
@@ -28,32 +39,35 @@ public class PresentationLayer {
             System.out.println("5.- Search by Genre");
             System.out.println("6.- Exit");
             System.out.println("Please select a choice");
-            var optionScanner = new Scanner(System.in);
-            userOption = optionScanner.nextLine();
+
+            String userOption1 = optionScanner.nextLine();
+            userOption = userOption1;
             if (!isValidOption(userOption)) {
                 System.out.println("Invalid option, please select a valid option");
             } else {
-                var value = Integer.parseInt(userOption);
+                Integer value = Integer.parseInt(userOption);
                 if (value == 1) {
-                    var dal = new WorkflowOneDal(connectionManager.getConnection());
+                    WorkflowOneDal dal = new WorkflowOneDal(connectionManager.getConnection());
                     dal.showStock();
                 } else if (value == 2){
-                    var dal = new WorkflowTwoDal(connectionManager.getConnection());
+                    WorkflowTwoDal dal = new WorkflowTwoDal(connectionManager.getConnection());
                     // call methods to purchase game
                     dal.purchaseGame();
                 }
                 else if (value == 3) {
-                    var dal = new WorkflowThreeDal(connectionManager.getConnection());
+                    WorkflowThreeDal dal = new WorkflowThreeDal(connectionManager.getConnection());
                     dal.showPurchasesHistory();
                 } else if (value == 4) {
-                    var dal = new WorkflowFourDal(connectionManager.getConnection());
+                    WorkflowFourDal dal = new WorkflowFourDal(connectionManager.getConnection());
                     dal.ListPopularGames();
                 } else if (value == 5) {
-                    var dal = new WorkflowFiveDal(connectionManager.getConnection());
+                    WorkflowFiveDal dal = new WorkflowFiveDal(connectionManager.getConnection());
                     dal.searchByGenre();
                 }
             }
+        
         }
+        optionScanner.close();
     }
 
     private static boolean isValidOption(String value) {
